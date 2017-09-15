b8d2ddbdc63bad5308e806286dc5d30fccf055b4
LucaAlvesNaveRio <llucca1999@gmail.com>
2017-09-15 12:03:48 -0300

    Tag Drawer
    Segunda avaliação de TPJ2

---
+++ Assets/Editor/TagSelectorDrawer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEditorInternal;
+
+[CustomPropertyDrawer(typeof(TagSelectorAttribute))]
+public class TagSelectorDrawer : PropertyDrawer
+{
+   
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        int index = 0;
+        string[] options = InternalEditorUtility.tags;
+
+        index = System.Array.IndexOf(options, property.stringValue);
+        
+        index = EditorGUI.Popup(position, index, options);
+
+        if (index >= 0)
+        {
+            property.stringValue = options[index];
+        }
+    }
+}
---
+++ Assets/TestBehaviour.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBehaviour : MonoBehaviour
+{
+    [TagSelector]
+    public string myTag;
+}
